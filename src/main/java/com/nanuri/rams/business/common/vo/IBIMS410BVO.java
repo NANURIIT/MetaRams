f5a3cd951ccdf1b893cd4657bdaa49327e9a2c97
kta <ky736905@naver.com>
2024-11-18 17:16:30 +0900

    [FIX] VO 수정

--- src/main/java/com/nanuri/rams/business/common/vo/IBIMS410BVO.java
+++ src/main/java/com/nanuri/rams/business/common/vo/IBIMS410BVO.java
@@ -14,21 +14,21 @@ import lombok.Setter;
  딜거래내역 Table.IBIMS410B VO
 */
 public class IBIMS410BVO extends IBIMS410BDTO {
-    String dealNo;                          // 딜번호
-    String icdcDcd;                         // 증감구분코드
+    private String dealNo;                          // 딜번호
+    private String icdcDcd;                         // 증감구분코드
 
-    String prdtNm;                          // 종목코드명
-    String etprCrdtGrntTrKindNm;            // 거래종류코드명
+    private String prdtNm;                          // 종목코드명
+    private String etprCrdtGrntTrKindNm;            // 거래종류코드명
 
-    String crryCd;                          // 통화코드
-    String holdPrpsDcdNm;
-    String trDptNm;
-    String rqsEmpNm;
-    String fndCd;                           // 펀드코드
-    String fndNm;                           // 펀드명
-    String fnltCd;                          // 외부기관코드
-    String fnltNm;                          // 외부기관명
-    BigDecimal stdrExrt;                    // 기준환율
+    private String crryCd;                          // 통화코드
+    private String holdPrpsDcdNm;
+    private String trDptNm;
+    private String rqsEmpNm;
+    private String fndCd;                           // 펀드코드
+    private String fndNm;                           // 펀드명
+    private String fnltCd;                          // 외부기관코드
+    private String fnltNm;                          // 외부기관명
+    private BigDecimal stdrExrt;                    // 기준환율
     
     private String trNm;                    // 거래명
     
@@ -58,16 +58,16 @@ public class IBIMS410BVO extends IBIMS410BDTO {
 
 
     //String dealNo; //딜번호
-    String dealNm; //딜명
-    String prdtMdclCd; //중분류코드
-    String rsltnDt; //조회시작일
-    String rsltnEndDt; //조회마감일
-    String bsnsRgstNo; //거래상대방
-    String prdtMdclNm; //중분류코드명
-    String trStatNm; //거래상태코드명
-    String rvseCnclDvsnNm; //정정취소구분코드명
-    String prgSttsCd; //진행상태코드
-    String prgSttsNm; //진행상태코드명
+    private String dealNm; //딜명
+    private String prdtMdclCd; //중분류코드
+    private String rsltnDt; //조회시작일
+    private String rsltnEndDt; //조회마감일
+    private String bsnsRgstNo; //거래상대방
+    private String prdtMdclNm; //중분류코드명
+    private String trStatNm; //거래상태코드명
+    private String rvseCnclDvsnNm; //정정취소구분코드명
+    private String prgSttsCd; //진행상태코드
+    private String prgSttsNm; //진행상태코드명
 
     
 
--- src/main/java/com/nanuri/rams/business/common/vo/IBIMS981BVO.java
+++ src/main/java/com/nanuri/rams/business/common/vo/IBIMS981BVO.java
@@ -15,7 +15,7 @@ public class IBIMS981BVO extends IBIMS981BDTO {
     private String dudtMngmDtldJobKndCd;   //  업무종류코드
     private String dealNm;                     //  딜명
     private String ardyBzepNo;              // 기업체번호
-    private String bzep_name;              // 기업체이름
+    //private String bzep_name;              // 기업체이름
     private String prevDate;
     private String nextDate;
 }
