77f47f7369626a70dce97ae6ae3ce28d435343db
kta <ky736905@naver.com>
2024-11-19 15:32:29 +0900

    [FIX] Junit test log 제거

--- src/test/java/com/nanuri/rams/RamsApplicationTests.java
+++ src/test/java/com/nanuri/rams/RamsApplicationTests.java
@@ -2,15 +2,15 @@ package com.nanuri.rams;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import lombok.extern.slf4j.Slf4j;
+//import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
+//@Slf4j
 @SpringBootTest
 public class RamsApplicationTests {
 
 	@Test
 	public void contextLoads() {
-		log.debug("############## contextLoads run ##############");
+		// log.debug("############## contextLoads run ##############");
 
 		
 
