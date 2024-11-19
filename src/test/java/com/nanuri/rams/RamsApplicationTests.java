cfc82414083743d361037e3b6cb2c92626c0107b
kgw <rlarjsdn75@gmail.com>
2024-11-19 15:13:45 +0900

    UPDATE

--- src/test/java/com/nanuri/rams/RamsApplicationTests.java
+++ src/test/java/com/nanuri/rams/RamsApplicationTests.java
@@ -1,13 +1,23 @@
 package com.nanuri.rams;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.nanuri.rams.business.assessment.tb06.tb06010.TB06010ServiceImpl;
+import com.nanuri.rams.business.common.dto.IBIMS202BDTO;
 
 @SpringBootTest
 class RamsApplicationTests {
 
+	@Autowired
+	private TB06010ServiceImpl tb06010ServiceImpl;
+
 	@Test
 	void contextLoads() {
+
+		
+
 	}
 
 }
