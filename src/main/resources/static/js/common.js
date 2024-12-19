8bb622dce3367436461841af1303002154713a3f
kgw <rlarjsdn75@gmail.com>
2024-12-19 15:46:43 +0900

    update

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -16,6 +16,7 @@ $(function () {
   setFileUploadEvent(url.split("/")[1]);
 
   $(`div[data-menuid="/${url.split("/")[1]}"] #key1`).val(url.split("/")[1]);
+  $(`div[data-menuid="/${url.split("/")[1]}"] #key1`).attr("name", "key1");
 
   if (
     $('script[src="js/business/tb/TB06011P.js"]').attr("src") ===
