c9be7abefeb5c2c9b8394cdd9887514b7639c069
kgw <rlarjsdn75@gmail.com>
2024-12-20 16:51:07 +0900

    update common.js, ramslayout.js

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -4,12 +4,11 @@
 // 전역 변수를 window 속성으로 정의 (중복 선언 방지)
 window.latestClickData = window.latestClickData || null;
 
-/** onload **/
-$(function () {
-  /**
-   * keydown, onchange 이벤트 생성
-   * @author {김건우}
-   */
+/**
+ * keydown, onchange 이벤트 생성
+ * @author {김건우}
+ */
+function settingFunction() {
 
   const url = window.location.pathname;
 
@@ -24,17 +23,17 @@ $(function () {
     $(".modal-dialog").draggable();
 
     $(document).on("mouseup.draggableDestroy", function () {
-        // draggable 비활성화
-        // $(".modal-dialog").draggable("destroy");
+      // draggable 비활성화
+      // $(".modal-dialog").draggable("destroy");
 
-        if ($(".modal-dialog").data("ui-draggable")) {
-          $(".modal-dialog").draggable("destroy");
+      if ($(".modal-dialog").data("ui-draggable")) {
+        $(".modal-dialog").draggable("destroy");
       }
 
-        // mouseup 이벤트 해제 (중복 방지)
-        $(document).off("mouseup.draggableDestroy");
+      // mouseup 이벤트 해제 (중복 방지)
+      $(document).off("mouseup.draggableDestroy");
 
-        console.log("드래그 비활성화 및 이벤트 해제 완료");
+      console.log("드래그 비활성화 및 이벤트 해제 완료");
     });
   });
 
@@ -116,42 +115,7 @@ $(function () {
   });
   $(".input-group.clockpicker").clockpicker({});
 
-  /**
-   * 심심해서 만들어봄 (폐기)
-   */
-  // $(document).on('keydown', function(event) {
-  // 	if (event.shiftKey && event.code === "Digit1") {
-  // 		moveTab($($('#myTab li')[0]).attr('data-tabid').split('/')[1]);
-  // 	}
-  // 	else if (event.shiftKey && event.code === "Digit2") {
-  // 		moveTab($($('#myTab li')[1]).attr('data-tabid').split('/')[1]);
-  // 	}
-  // 	else if (event.shiftKey && event.code === "Digit3") {
-  // 		moveTab($($('#myTab li')[2]).attr('data-tabid').split('/')[1]);
-  // 	}
-  // 	else if (event.shiftKey && event.code === "Digit4") {
-  // 		moveTab($($('#myTab li')[3]).attr('data-tabid').split('/')[1]);
-  // 	}
-  // 	else if (event.shiftKey && event.code === "Digit5") {
-  // 		moveTab($($('#myTab li')[4]).attr('data-tabid').split('/')[1]);
-  // 	}
-  // 	else if (event.shiftKey && event.code === "Digit6") {
-  // 		moveTab($($('#myTab li')[5]).attr('data-tabid').split('/')[1]);
-  // 	}
-  // 	else if (event.shiftKey && event.code === "Digit7") {
-  // 		moveTab($($('#myTab li')[6]).attr('data-tabid').split('/')[1]);
-  // 	}
-  // 	else if (event.shiftKey && event.code === "Digit8") {
-  // 		moveTab($($('#myTab li')[7]).attr('data-tabid').split('/')[1]);
-  // 	}
-  // 	else if (event.shiftKey && event.code === "Digit9") {
-  // 		moveTab($($('#myTab li')[8]).attr('data-tabid').split('/')[1]);
-  // 	}
-  // 	else if (event.shiftKey && event.code === "Digit0") {
-  // 		moveTab($($('#myTab li')[9]).attr('data-tabid').split('/')[1]);
-  // 	}
-  // });
-});
+}
 
 /**
  * 비동기 통신함수
@@ -1774,38 +1738,6 @@ function newAddMonth(d, n) {
 /*************************************************************************
  * common ui js						                                     *
  *************************************************************************/
-// 내부 content 높이 가져오기 (내부 스크롤)
-$(function () {
-  // let innerH = window.innerHeight;
-  // let navH = $('.navbar').height();
-  // let headerH = $('.page-heading').height();
-  // let tabH = $('.top-nav-tabs').height();
-  // let footerH = $('.footer').height();
-  // $('.wrapper-content').height(innerH - navH - tabH - headerH - footerH - 62 + 'px');
-  // $(window).resize(function () {
-  // 	let innerH = window.innerHeight;
-  // 	let navH = $('.navbar').height();
-  // 	let headerH = $('.page-heading').height();
-  // 	let footerH = $('.footer').height();
-  // 	$('.wrapper-content').height(innerH - navH - tabH - headerH - footerH - 62 + 'px');
-  // });
-
-  // 모달 드래그
-  $(function () {
-
-  });
-
-  // 모달 사이즈 조절
-  // $('.modal-dialog').on('show.bs.modal', function () {
-  // 	if( option.maxWidth !== ""){
-  // 		$(this).find('.modal-dialog > .modal-content').attr('style', 'max-width: ' + option.maxWidth + 'px;'); // modal.scss의 max-width를 override
-  // 	}
-  // 	$(this).find('.modal-body > .modal-content').css({'max-height':'100%'});
-  // 	if( option.contentHref !== ""){
-  // 		$(this).find(".modal-body").load(option.contentHref);
-  // 	}
-  // });
-});
 
 // 초기화
 // 페이지 로드 시, 입력 요소의 초기 값
