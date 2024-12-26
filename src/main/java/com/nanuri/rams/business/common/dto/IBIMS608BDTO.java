08171d8faa684c177fdb0afd84801c18d401d272
jihyunk <kjh56900@gmail.com>
2024-12-26 11:02:02 +0900

    [UPDATE]TB8010S파일첨부 및 데이퍼 매핑

--- src/main/java/com/nanuri/rams/business/common/dto/IBIMS608BDTO.java
+++ src/main/java/com/nanuri/rams/business/common/dto/IBIMS608BDTO.java
@@ -13,18 +13,22 @@ import java.util.Date;
 */
 public class IBIMS608BDTO {
     
-    private String dealNo;              //  딜번호
+    private String dealNo;              //  딜번호    
+    private String ibDealNo;              //  딜번호    
     private String jdgmDcd;             //  심사구분코드
     private String mtrDcd;              //  부수안건구분코드
     private int    sn;                  //  일련번호
     private String lglPrcrCcd;          //  법적절차구분코드
+    private String lglPrcrCcdNm;          //  법적절차구분
     private String lglPrcrKndCcd;       //  법적절차종류구분코드
+    private String lglPrcrKndCcdNm;       //  법적절차종류구분
     private String lglPrcrCntnt;        //  법적절차내용
     private String crtrmInfo;           //  법원정보
     private String acdntNo;             //  사건번호
     private String rgstDt;              //  등록일자
     private String rgstTm;              //  등록시간
     private String fstRgstPEno;         //  최초등록자사번
+    private String fstRgstPEnoNm;         //  최초등록자
     private Date hndlDyTm;              //  처리일시
     private String hndlDprtCd;          //  처리부서코드
     private String hndlPEno;            //  처리자사번
