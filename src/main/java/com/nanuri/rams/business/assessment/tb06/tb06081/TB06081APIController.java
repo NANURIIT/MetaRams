1ada66acc6133148488fab9bb0ac323414fe8454
kgw <rlarjsdn75@gmail.com>
2025-01-06 12:45:49 +0900

    결재관리

--- src/main/java/com/nanuri/rams/business/assessment/tb06/tb06081/TB06081APIController.java
+++ src/main/java/com/nanuri/rams/business/assessment/tb06/tb06081/TB06081APIController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.nanuri.rams.business.common.dto.IBIMS003BDTO;
 import com.nanuri.rams.business.common.dto.IBIMS231BDTO;
 import com.nanuri.rams.business.common.vo.IBIMS003BVO;
+import com.nanuri.rams.business.common.vo.IBIMS231BVO;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -33,14 +34,24 @@ public class TB06081APIController {
 	}
 	
 	/**
-	 * 결재요청
+	 * 결재
 	 * @param paramData
 	 * @return
 	 */
-	// @PostMapping("/saveApvl")
-	// public List<IBIMS003BVO> saveApvl(@RequestBody IBIMS003BVO paramData) {
-	// 	return tb06081Service.saveApvl(paramData);
-	// }
+	@PostMapping("/apvlRqst")
+	public int apvlRqst(@RequestBody IBIMS231BVO paramData) {
+		return tb06081Service.apvlRqst(paramData);
+	}
+
+	/**
+	 * 결재취소 요청
+	 * @param paramData
+	 * @return
+	 */
+	@PostMapping("/cancelApvlRqst")
+	public int cancelApvlRqst(@RequestBody IBIMS231BVO paramData) {
+		return tb06081Service.cancelApvlRqst(paramData);
+	}
 
 	/**
 	 * 결재요청내역 확인
--- src/main/java/com/nanuri/rams/business/common/dto/IBIMS231BDTO.java
+++ src/main/java/com/nanuri/rams/business/common/dto/IBIMS231BDTO.java
@@ -8,34 +8,33 @@ import lombok.Setter;
 
 @Getter
 @Setter
-/* 
- 딜승인결재기본 - 결재내역조회(TB06080S) - Table.IBIMS231B DTO
-*/
+/*
+ * 딜승인결재기본 - 결재내역조회(TB06080S) - Table.IBIMS231B DTO
+ */
 public class IBIMS231BDTO {
 
-	private BigDecimal	decdSn;			/* 결재일련번호		*/
-	private BigDecimal	apvlRqstSq;		/* 승인요청순번		*/
-	private String		chrrEno;		/* 책임자사번		*/
-	private String		apvlRqstPEno;	/* 승인요청자사번	*/
-	private String		decdStepDcd;	/* 결재단계구분코드	*/
-	private String		decdSttsDcd;	/* 결재상태구분코드	*/
-	private String		dealNo;			/* 딜번호			*/
-	private String		prdtCd;			/* 종목코드			*/
-	private String		decdJobDcd;		/* 결재업무구분코드	*/
-	private String		scrnNo;			/* 화면번호		*/
-	private String		apvlRqstCntn;	/* 승인요청내용		*/
-	private Date		rqstDtm;		/* 신청일시		*/
-	private Date		rqstCnclDtm;	/* 신청취소일시		*/
-	private String		prcsRsltDcd;	/* 처리결과구분코드	*/
-	private BigDecimal	excSeq;			/* 실행순번		*/
-	private BigDecimal	rqstSq;			/* 신청순번		*/
-	private BigDecimal	trSeq;			/* 거래순번		*/
-	private String		errCntn;		/* 오류내용		*/
-	private BigDecimal	lastDecdSq;		/* 최종결재순번		*/
-	private Date		hndDetlDtm;		/* 조작상세일시		*/
-	private String		hndEmpno;		/* 조작사원번호		*/
-	private String		hndTmnlNo;		/* 조작단말기번호	*/
-	private String		hndTrId;		/* 조작거래id		*/
-	private String		guid;			/* guid			*/
+	private int decdSn;				/* 결재일련번호 */		
+	private String chrrEno;			/* 책임자사번 */			
+	private String apvlRqstPEno;	/* 승인요청자사번 */					
+	private String decdStepDcd;		/* 결재단계구분코드 */				
+	private String decdSttsDcd;		/* 결재상태구분코드 */				
+	private String dealNo;			/* 딜번호 */			
+	private String prdtCd;			/* 종목코드 */			
+	private String decdJobDcd;		/* 결재업무구분코드 */				
+	private String scrnNo;			/* 화면번호 */			
+	private String apvlRqstCntn;	/* 승인요청내용 */					
+	private Date rqstDtm;			/* 신청일시 */			
+	private Date rqstCnclDtm;		/* 신청취소일시 */				
+	private String prcsRsltDcd;		/* 처리결과구분코드 */				
+	private int excSeq;				/* 실행순번 */		
+	private int rqstSq;				/* 신청순번 */		
+	private int trSeq;				/* 거래순번 */		
+	private String errCntn;			/* 오류내용 */			
+	private int lastDecdSq;			/* 최종결재순번 */			
+	private Date hndDetlDtm;		/* 조작상세일시 */				
+	private String hndEmpno;		/* 조작사원번호 */				
+	private String hndTmnlNo;		/* 조작단말기번호 */				
+	private String hndTrId;			/* 조작거래id */			
+	private String guid;			/* guid */			
 
 }
