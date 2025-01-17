6c273756653e42551f748f9aec44798cf6a7ed65
kgw <rlarjsdn75@gmail.com>
2025-01-17 15:15:57 +0900

    자금품의결재

--- src/main/java/com/nanuri/rams/business/common/mapper/IBIMS452BMapper.java
+++ src/main/java/com/nanuri/rams/business/common/mapper/IBIMS452BMapper.java
@@ -4,15 +4,18 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
-import com.nanuri.rams.business.common.vo.IBIMS452BVO;
+import com.nanuri.rams.business.common.dto.IBIMS452BDTO;
 
 @Mapper
 public interface IBIMS452BMapper {
 
     // 최초 승인 신청시 정보 입력
-    public int insertFndsCnstDecd(IBIMS452BVO param);
+    public int insertFndsCnstDecd(IBIMS452BDTO param);
 
     // 추후 승인 신청, 반려, 취소 업데이트
-    public int updateFndsCnstDecd(IBIMS452BVO param);
+    public int updateFndsCnstDecd(IBIMS452BDTO param);
+
+    // 등록순번 채번
+    public int getErlmSeq();
 
 }
