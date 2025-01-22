c7cf5a3f91b17fabeefa9529d4118247e0b8974c
kgw <rlarjsdn75@gmail.com>
2025-01-22 15:39:19 +0900

    지급품의서 중간저장

--- src/main/java/com/nanuri/rams/business/common/mapper/IBIMS431BMapper.java
+++ src/main/java/com/nanuri/rams/business/common/mapper/IBIMS431BMapper.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.nanuri.rams.business.common.dto.IBIMS431BDTO;
 import com.nanuri.rams.business.common.vo.IBIMS431BVO;
 
 @Mapper
@@ -12,13 +13,15 @@ public interface IBIMS431BMapper {
     // 지급품의목록
     public List<IBIMS431BVO> selectIBIMS431B(IBIMS431BVO param);
 
-    public int insertIBIMS431B (IBIMS431BVO param);
+    public int insertIBIMS431B (IBIMS431BDTO param);
 
-    public int updateIBIMS431B (IBIMS431BVO param);
+    public int updateIBIMS431B (IBIMS431BDTO param);
 
     public int apvlRqst (IBIMS431BVO param);
 
     public int deleteIBIMS431B (IBIMS431BVO param);
 
     public String getCnstNo (IBIMS431BVO param);
+
+    public String setCnstNo (String param);
 }
