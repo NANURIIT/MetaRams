74cf8e744a12a90515b2a9e34326231de92be41a
nanuri <nanuri@DESKTOP-B8M3T7M>
2025-02-06 17:32:32 +0900

    [fix] 배치 제거

--- src/main/java/com/nanuri/rams/RamsApplication.java
+++ src/main/java/com/nanuri/rams/RamsApplication.java
@@ -4,7 +4,6 @@ import java.util.TimeZone;
 
 import javax.annotation.PostConstruct;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -13,7 +12,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @EnableScheduling
-@EnableBatchProcessing
 @SpringBootApplication
 public class RamsApplication {
 	
