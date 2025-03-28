622ad22f626a698010a8aff971a5225d59e37674
kgw <rlarjsdn75@gmail.com>
2025-03-28 10:13:17 +0900

    IBIMS900BMapper, xml 업로드

---
+++ src/main/java/com/nanuri/rams/business/common/mapper/IBIMS900BMapper.java
@@ -0,0 +1,12 @@
+package com.nanuri.rams.business.common.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface IBIMS900BMapper {
+
+    
+
+}
