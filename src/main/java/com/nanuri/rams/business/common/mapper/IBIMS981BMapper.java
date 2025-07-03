709bbd258f5fab36ad2996e91b4114cf998b2397
김주연 <goldmom7@naver.com>
2025-07-03 16:27:52 +0900

    기일관리내역생성 sql관련 수정

--- src/main/java/com/nanuri/rams/business/common/mapper/IBIMS981BMapper.java
+++ src/main/java/com/nanuri/rams/business/common/mapper/IBIMS981BMapper.java
@@ -3,14 +3,16 @@ package com.nanuri.rams.business.common.mapper;
 import org.apache.ibatis.annotations.Mapper;
 import java.util.List;
 
+import com.nanuri.rams.business.common.dto.IBIMS981BDTO;
 import com.nanuri.rams.business.common.vo.IBIMS981BVO;
 
 @Mapper
 public interface IBIMS981BMapper {
-    
+
     public List<IBIMS981BVO> selectIBIMS981B(IBIMS981BVO param);
 
-    public int batchInsertIBIMS981B(String hndEmpno);
+    // public int batchInsertIBIMS981B(String hndEmpno);
+    public int batchInsertIBIMS981B(IBIMS981BDTO param);
 
     public int batchDeleteIBIMS981B(String stdrDt);
 
